cc7804ffb71d200d1b7619d1b22cda917975701d
Aric Akbani <aric@arro.works>
2026-06-03 22:56:08 -0500

    Variable Jump added

--- assets/scripts/player_character.gd
+++ assets/scripts/player_character.gd
@@ -1,8 +1,11 @@
-class_name PlayerController extends CharacterBody2D
+class_name PlayerCharacter extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export_category("Speed")
+@export var speed : float = 300
+@export var acceleration : float = 10
+@export var deceleration : float = 10
+@export_category("Jump Handling")
+@export var jump_velocity : float = -300
 @export var coyoteWaitTime: float = 0.15
 @onready var coyote_timer: Timer = $"../CoyoteTimer"
 var is_coyote_time: bool = false
@@ -28,20 +31,23 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		can_jump = true
 		handle_jump()
+	elif velocity.y < 0.0:
+		if Input.is_action_just_released("a_button"):
+			velocity.y *= 0.5
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("d_pad_left", "d_pad_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 
 func handle_jump():
 	if Input.is_action_just_pressed("a_button") and (can_jump or is_coyote_time):
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 		# prevent infinite jumping
 		# disable if you want to add double jumping
 		can_jump = false
